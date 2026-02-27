5b470cbe6759a4203ebcda18d179de02e0882c90
davidced25 <david2250933@correo.uis.edu.co>
2026-02-27 00:12:43 -0500

    Update base-LSE.py

--- base-LSE.py
+++ base-LSE.py
@@ -5,42 +5,125 @@
 
 # Clase Nodo
 class Nodo:
-	def __init__(self, data):
-		self.data = data
-		self.siguiente = None
+    def __init__(self, data):
+        self.data = data
+        self.siguiente = None
 
-# CLase Listas enlazada simple
+
+# Clase Lista enlazada simple
 class ListaSE:
-	def __init__(self):
-		self.cabeza = None
-  
-  	# Lista Vacia
-	def vacio(self):
-		if self.cabeza == None:
-			print("Está vacia")
-		else:
-			print("Lista no vacia")
+    def __init__(self):
+        self.cabeza = None
 
-	# Agregar al inicio
-	def agregarInicio(self, data):
-		nuevo_nodo = Nodo(data)
-		if self.cabeza is None:
-			self.cabeza = nuevo_nodo
-			return
-		else:
-			nuevo_nodo.siguiente = self.cabeza
-			self.cabeza = nuevo_nodo
+    # Verifica si la lista esta vacia
+    def vacio(self):
+        if self.cabeza is None:
+            print("Esta vacia")
+        else:
+            print("Lista no vacia")
 
-#agregar al final
-def agregarfin(self, data):
-    nuevo_nodo = Nodo(data)
-    if self.cabeza is None:
+    # Insertar al inicio
+    def agregarInicio(self, data):
+        nuevo_nodo = Nodo(data)
+        nuevo_nodo.siguiente = self.cabeza
         self.cabeza = nuevo_nodo
-        return
-    else:
+
+    # Insertar al final
+    def agregarFinal(self, data):
+        nuevo_nodo = Nodo(data)
+
+        if self.cabeza is None:
+            self.cabeza = nuevo_nodo
+            return
+
         actual = self.cabeza
         while actual.siguiente is not None:
             actual = actual.siguiente
+
         actual.siguiente = nuevo_nodo
- 
+
+    # Insertar antes de un elemento X
+    def insertarAntes(self, x, data):
+        if self.cabeza is None:
+            print("Lista vacia")
+            return
+
+        # Si el primero es el que buscamos
+        if self.cabeza.data == x:
+            self.agregarInicio(data)
+            return
+
+        actual = self.cabeza
+        while actual.siguiente is not None:
+            if actual.siguiente.data == x:
+                nuevo_nodo = Nodo(data)
+                nuevo_nodo.siguiente = actual.siguiente
+                actual.siguiente = nuevo_nodo
+                return
+            actual = actual.siguiente
+
+        print("Elemento no encontrado")
+
+    # Insertar despues de un elemento X
+    def insertarDespues(self, x, data):
+        actual = self.cabeza
+
+        while actual is not None:
+            if actual.data == x:
+                nuevo_nodo = Nodo(data)
+                nuevo_nodo.siguiente = actual.siguiente
+                actual.siguiente = nuevo_nodo
+                return
+            actual = actual.siguiente
+
+        print("Elemento no encontrado")
+
+    # Eliminar el primero
+    def eliminarPrimero(self):
+        if self.cabeza is None:
+            print("Lista vacia")
+            return
+
+        self.cabeza = self.cabeza.siguiente
+
+    # Eliminar el ultimo
+    def eliminarUltimo(self):
+        if self.cabeza is None:
+            print("Lista vacia")
+            return
+
+        # Si solo hay un nodo
+        if self.cabeza.siguiente is None:
+            self.cabeza = None
+            return
+
+        actual = self.cabeza
+        while actual.siguiente.siguiente is not None:
+            actual = actual.siguiente
+
+        actual.siguiente = None
+
+    # Buscar elemento por valor
+    def buscar(self, valor):
+        actual = self.cabeza
+
+        while actual is not None:
+            if actual.data == valor:
+                print("Verdadero")
+                return
+            actual = actual.siguiente
+
+        print("Falso")
+
+    # Contar elementos
+    def contar(self):
+        cont = 0
+        actual = self.cabeza
+
+        while actual is not None:
+            cont += 1
+            actual = actual.siguiente
+
+        print("Cantidad de elementos:", cont)
+
 
